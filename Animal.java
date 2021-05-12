94b304c70cdd64b0ab76d296aaaf7ae30bf2562d
hapsior <hapkad@gmail.com>
2021-05-12 18:47:28 +0200

    Zadanie 1
    Utworzono klasy i metody

---
+++ Animal.java
@@ -0,0 +1,54 @@
+package com.company;
+
+public class Animal {
+    final public String species;
+    private Double weight;
+    String name;
+
+    public Animal(String species) {
+        this.species = species;
+        switch (this.species) {
+            case "dog":
+                this.weight = 10.2;
+                break;
+            case "cat":
+                this.weight = 2.0;
+                break;
+            case "mouse":
+                this.weight = 0.01;
+                break;
+            default:
+                this.weight = 1.0;
+        }
+    }
+
+    void feed() {
+        if (this.weight > 0) {
+            this.weight *= 1.1;
+            System.out.println("thx for food");
+        } else {
+            System.out.println("Now it is too late!");
+        }
+    }
+
+    void takeForAWalk() {
+        if (this.weight > 0) {
+            this.weight -= 1.0;
+            System.out.println("Thx for a walk, my weight now is: " + this.weight);
+        } else {
+            System.out.println("Now it is too late!");
+        }
+    }
+
+    Double getWeight() {
+        return this.weight;
+    }
+
+    void printName() {
+        System.out.println(this.name);
+    }
+
+    void printNameAndOwner(String owner) {
+        System.out.println(owner + " has " + this.name);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Animal dog = new Animal("dog");
+
+        dog.name = "Szarik";
+
+        dog.feed();
+        dog.takeForAWalk();
+        dog.takeForAWalk();
+        dog.takeForAWalk();
+        dog.feed();
+        dog.feed();
+        dog.feed();
+        dog.feed();
+        dog.feed();
+        dog.feed();
+        dog.feed();
+        dog.feed();
+        dog.takeForAWalk();
+        dog.takeForAWalk();
+        dog.takeForAWalk();
+        dog.takeForAWalk();
+       /* System.out.println(dog.name);
+        System.out.println(dog.species);
+        dog.printName();
+        dog.printNameAndOwner("Damian");
+        Human damian = new Human();
+        damian.firstName = "Damian";
+        damian.lastName = "Hapka";
+        damian.pet = dog;
+
+        System.out.println(damian.pet.species); */
+    }
+}
